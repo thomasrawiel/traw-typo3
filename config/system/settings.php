7873b76de69ff330651ed7bbace1b04dd75b5e56
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2025-01-31 09:26:33 +0100

    default to typo3 13

--- config/system/settings.php
+++ config/system/settings.php
@@ -2,7 +2,9 @@
 return [
     'BE' => [
         'debug' => true,
-        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$RDhRc1o0MG1ic1c3Y0drcQ$5ZvFB4QQwRWN6bz0tfvXtKSeM7C127C+a/xLWJbzYHw',
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$WE9YaE5oaDdHS1hKZUZYag$73dWt7+DQt5AJW3AqYILNLYQKwdI15wDFciCXHN9bIo',
+        'lockRootPath' => '/var/www/html/',
+        'lockSSL' => true,
         'passwordHashing' => [
             'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
             'options' => [],
@@ -13,14 +15,14 @@ return [
             'Default' => [
                 'charset' => 'utf8mb4',
                 'dbname' => 'db',
+                'defaultTableOptions' => [
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_unicode_ci',
+                ],
                 'driver' => 'mysqli',
                 'host' => 'db',
                 'password' => 'db',
                 'port' => 3306,
-                'tableoptions' => [
-                    'charset' => 'utf8mb4',
-                    'collate' => 'utf8mb4_unicode_ci',
-                ],
                 'user' => 'db',
             ],
         ],
@@ -39,68 +41,30 @@ return [
             'automaticInstallation' => '1',
             'offlineMode' => '0',
         ],
-        'powermail' => [
-            'disableBackendModule' => '0',
-            'disableIpLog' => '1',
-            'disableMarketingInformation' => '0',
-            'disablePluginInformation' => '0',
-            'disablePluginInformationMailPreview' => '0',
-            'enableCaching' => '0',
-            'extender' => [
-                'In2code\Powermail\Domain\Model\Field' => [
-                    'powermailautocomplete' => 'TRAW\\Powermailautocomplete\\Domain\\Model\\Field',
-                ],
-                'In2code\Powermail\Domain\Model\Form' => [
-                    'powermailautocomplete' => 'TRAW\\Powermailautocomplete\\Domain\\Model\\Form',
-                ],
-            ],
-            'replaceIrreWithElementBrowser' => '0',
+        'redirects' => [
+            'showCheckIntegrityInfoInReports' => '1',
+            'showCheckIntegrityInfoInReportsSeconds' => '86400',
         ],
         'scheduler' => [
             'maxLifetime' => '1440',
         ],
-        'staticfilecache' => [
-            'backendDisplayMode' => 'both',
-            'boostMode' => '0',
-            'cacheTagsEnable' => '0',
-            'clearCacheForAllDomains' => '1',
-            'debugHeaders' => '0',
-            'disableInDevelopment' => '0',
-            'enableGeneratorBrotli' => '0',
-            'enableGeneratorGzip' => '1',
-            'enableGeneratorManifest' => '0',
-            'enableGeneratorPhp' => '0',
-            'enableGeneratorPlain' => '0',
-            'hashUriInCache' => '0',
-            'htaccessTemplateName' => 'EXT:staticfilecache/Resources/Private/Templates/Htaccess.html',
-            'inlineAssetsFileSize' => '50000',
-            'inlineScriptMinify' => '0',
-            'inlineServiceFavIcon' => '0',
-            'inlineServiceScripts' => '0',
-            'inlineServiceStyles' => '0',
-            'inlineStyleAssets' => 'ico,png,woff2',
-            'inlineStyleMinify' => '0',
-            'largeIdentifierInCacheTable' => '0',
-            'overrideCacheDirectory' => '',
-            'phpTemplateName' => 'EXT:staticfilecache/Resources/Private/Templates/Php.html',
-            'rawurldecodeCacheFileName' => '0',
-            'renameTablesToOtherPrefix' => '0',
-            'sendCacheControlHeaderRedirectAfterCacheTimeout' => '0',
-            'sendHttp2PushEnable' => '0',
-            'sendHttp2PushFileExtensions' => 'css,js',
-            'sendHttp2PushFileLimit' => '10',
-            'sendHttp2PushLimitToArea' => '',
-            'useFallbackMiddleware' => '1',
-            'useReverseUriLengthInPriority' => '1',
-            'validFallbackHeaders' => 'Content-Type,Content-Language,Content-Security-Policy,Link,X-SFC-Tags',
-            'validHtaccessHeaders' => 'Content-Type,Content-Language,Content-Security-Policy,Link,X-SFC-Tags',
+        'tt_address' => [
+            'readOnlyNameField' => '1',
+            'storeBackwardsCompatName' => '1',
+            'telephoneValidationPatternForJs' => '/[^\\d\\+\\s\\-]/g',
+            'telephoneValidationPatternForPhp' => '/[^\\d\\+\\s\\-]/',
+        ],
+        'vcfqr' => [
+            'addressTablename' => 'tt_address',
+            'enableExamples' => '1',
+            'eventTablename' => 'tt_content',
+            'qrcodeCacheLifetime' => '86400',
+            'qrfolder' => 'qr-codes',
+            'storageUid' => '0',
+            'vcardVersion' => '5',
         ],
         'vhs_col' => [
-            'enableGalleryProcessor' => '0',
-        ],
-        'whatchado' => [
-            'whatchadoApiKey' => '',
-            'whatchadoApiUrl' => 'https://whatchado.com/api2/videostories/',
+            'enableGalleryProcessor' => '1',
         ],
     ],
     'FE' => [
@@ -116,8 +80,6 @@ return [
     ],
     'GFX' => [
         'processor' => 'GraphicsMagick',
-        'processor_allowTemporaryMasksAsPng' => false,
-        'processor_colorspace' => 'RGB',
         'processor_effects' => false,
         'processor_enabled' => true,
         'processor_path' => '/usr/bin/',
@@ -152,12 +114,6 @@ return [
                 'hash' => [
                     'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
                 ],
-                'imagesizes' => [
-                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
-                    'options' => [
-                        'compression' => true,
-                    ],
-                ],
                 'pages' => [
                     'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
                     'options' => [
@@ -174,15 +130,11 @@ return [
         ],
         'devIPmask' => '*',
         'displayErrors' => 1,
-        'encryptionKey' => 'a512dcbeb5bb16622563511c3a2b16b5cc6206f518684f2c0f07f35a81e746263604287e62299f127f093924289db55a',
+        'encryptionKey' => '7caa551b41187a47c87c9fe93232982bd5692669c896df002d12f780549a894c03fbda2a79154e91b6adbb9d16f4f16c',
         'exceptionalErrors' => 12290,
         'features' => [
-            'security.backend.enforceContentSecurityPolicy' => true,
-            'security.usePasswordPolicyForFrontendUsers' => true,
+            'frontend.cache.autoTagging' => true,
         ],
-        'sitename' => 'TRAW Test Instance',
-        'systemMaintainers' => [
-            1,
-        ],
+        'sitename' => 'New TYPO3 site',
     ],
 ];
